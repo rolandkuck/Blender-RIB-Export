835e5e1879ac0d870717597d6dba230ced7dcb5b
Roland Kuck <blenderrib@roland.kuck.name>
2007-02-28 02:13:09 +0200

    Refactoring to remove bad smell

--- parse.py
+++ parse.py
@@ -3,95 +3,93 @@ import string
 class ParseError(object):
     pass
 
+
+def find_pos(input, pos, predicate):
+    while predicate(input[pos]):
+        pos += 1
+        if pos >= len(input):
+            break
+    return pos
+
+class LineAssembler(object):
+    def __init__(self):
+        self.result = []
+        self.linecache = []
+
+    def insert(self, token):
+        self.linecache.append(token)
+
+    def flush(self):
+        if len(self.linecache) != 0:
+            self.result.append(self.linecache)
+        self.linecache = []
+
+    def retrieve(self):
+        self.flush()
+        return self.result
+
+
 class Name(object):
     delimiter = string.whitespace+'"[]#'
     def detect(self, input, pos):
         return input[pos] not in self.delimiter
-    def tokenize(self, input, pos):
+    def tokenize(self, input, pos, la):
         start = pos
-        while input[pos] not in self.delimiter:
-            pos += 1
-            if pos >= len(input):
-                break
-        return pos, input[start:pos]
+        pos = find_pos(input, pos, lambda x: x not in self.delimiter)
+        name = input[start:pos]
+        try:
+            value = float(name)
+        except:
+            la.flush()
+            value = name
+        la.insert(value)
+        return pos 
 
 class Whitespace(object):
     delimiter = string.whitespace
     def detect(self, input, pos):
         return input[pos] in self.delimiter
-    def tokenize(self, input, pos):
-        start = pos
-        while input[pos] in self.delimiter:
-            pos += 1
-            if pos >= len(input):
-                break
-        return pos, ""
+    def tokenize(self, input, pos, la):
+        pos = find_pos(input, pos, lambda x: x in self.delimiter)
+        return pos
 
 class Comment(object):
     delimiter = string.whitespace
     def detect(self, input, pos):
         return input[pos] == '#'
-    def tokenize(self, input, pos):
-        start = pos
-        while input[pos] != '\n':
-            pos += 1
-            if pos >= len(input):
-                break
-        return pos, ""
+    def tokenize(self, input, pos, la):
+        pos = find_pos(input, pos, lambda x: x != '\n')
+        return pos
 
 class String(object):
     def detect(self, input, pos):
         return input[pos] == '"'
-    def tokenize(self, input, pos):
+    def tokenize(self, input, pos, la):
         pos += 1
         if pos >= len(input):
             raise ParseError()
         start = pos
-        while input[pos] != '"':
-            pos += 1
-            if pos >= len(input):
-                break
-        return pos+1, input[start:pos]
+        pos = find_pos(input, pos, lambda x: x != '"')
+        la.insert(input[start:pos])
+        return pos+1
 
 class Array(object):
     def detect(self, input, pos):
         return input[pos] in '[]'
-    def tokenize(self, input, pos):
-        return pos+1, input[pos]
+    def tokenize(self, input, pos, la):
+        la.insert(input[pos])
+        return pos+1
+
 
 def parse(input):
-    tokens = []
-    w = Whitespace()
-    n = Name()
-    s = String()
-    a = Array()
-    c = Comment()
-    line = []
+    la = LineAssembler()
+    tokenizers = [ Whitespace(), Name(), String(), Array(), Comment() ]
     pos = 0
     while True:
         if pos >= len(input):
             break
-        if w.detect(input, pos):
-            pos, dummy = w.tokenize(input, pos)
-        if c.detect(input, pos):
-            pos, dummy = c.tokenize(input, pos)
-        elif s.detect(input, pos):
-            pos, value = s.tokenize(input, pos)
-            line.append(value)
-        elif a.detect(input, pos):
-            pos, value = a.tokenize(input, pos)
-            line.append(value)
-        elif n.detect(input, pos):
-            pos, name = n.tokenize(input, pos)
-            try:
-                value = float(name)
-                line.append(value)
-            except:
-                if len(line) != 0:
-                    tokens.append(line)
-                line = [name]
-        else:
-            break
-    if len(line) != 0:
-        tokens.append(line)
-    return tokens
+        for t in tokenizers:
+            if t.detect(input, pos):
+                pos = t.tokenize(input, pos, la)
+                break
+    return la.retrieve()
--- test_parse.py
+++ test_parse.py
@@ -3,37 +3,37 @@ import parse
 
 class TestParse(unittest.TestCase):
 
-    def testParseNames(self):
+    def testNames(self):
         in_str = "One Two Three"
         in_tokens = [ ["One"], ["Two"], ["Three"] ]
         self.assertEqual(parse.parse(in_str), in_tokens)
 
-    def testParseNames_Space(self):
+    def testNames_Space(self):
         in_str = " One   Two  Three"
         in_tokens = [ ["One"], ["Two"], ["Three"] ]
         self.assertEqual(parse.parse(in_str), in_tokens)
 
-    def testParseStrings(self):
+    def testStrings(self):
         in_str = ' One "Two"  Three "Four"'
         in_tokens = [ ["One", "Two"], ["Three", "Four"] ]
         self.assertEqual(parse.parse(in_str), in_tokens)
 
-    def testParseNumber(self):
+    def testNumber(self):
         in_str = 'Option "limits" 6'
         in_tokens = [ ["Option", "limits", 6 ] ]
         self.assertEqual(parse.parse(in_str), in_tokens)
         
-    def testParseArray(self):
+    def testArray(self):
         in_str = 'Option "limits" "bucketsize" [6 6]'
         in_tokens = [ ["Option", "limits", "bucketsize", '[', 6, 6, ']' ] ]
         self.assertEqual(parse.parse(in_str), in_tokens)
 
-    def testParseArray(self):
+    def testArray(self):
         in_str = 'One Two #Three\n Four'
         in_tokens = [ ["One"], ["Two"], ["Four"] ]
         self.assertEqual(parse.parse(in_str), in_tokens)
 
-    def testParseComplex(self):
+    def testComplex(self):
         in_str = """Declare "squish" "uniform float"
 Option "limits" "bucketsize" [6 6] #renderer specific
 WorldBegin"""
@@ -44,8 +44,29 @@ WorldBegin"""
         self.assertEqual(parse.parse(in_str), in_tokens)
 
 
+class TestLineAssembler(unittest.TestCase):
+
+    def testRetrieve(self):
+        la = parse.LineAssembler()
+        la.insert("A")
+        la.insert("B")
+        result = la.retrieve()
+        self.assertEqual(result, [ ["A", "B"] ])
+
+    def testRetrieve(self):
+        la = parse.LineAssembler()
+        la.insert("A")
+        la.insert("B")
+        la.flush()
+        la.insert("C")
+        result = la.retrieve()
+        self.assertEqual(result, [ ["A", "B"], ["C"] ])
+
+
 def retrieveTestSuite():
-    return unittest.TestLoader().loadTestsFromTestCase(TestParse) 
+    suiteParse = unittest.TestLoader().loadTestsFromTestCase(TestParse)
+    suiteLineAssembler = unittest.TestLoader().loadTestsFromTestCase(TestLineAssembler)
+    return [suiteParse, suiteLineAssembler]
 
 if __name__ == '__main__':
     unittest.main()
