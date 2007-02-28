108c3e3f6057484bccc8066cb9caf2c2864b78ab
Roland Kuck <blenderrib@roland.kuck.name>
2007-02-28 22:49:10 +0200

    Added Formatter object with wrapping behaviour

--- rib.py
+++ rib.py
@@ -1,7 +1,63 @@
 class RIB(object):
+
     def __init__(self, hout):
         self.hout = hout
+
     def output(self, token):
         pass
+
     def close(self):
         print >> self.hout, "WorldBegin"
+
+
+class Formatter(object):
+
+    def __init__(self, hout):
+        self.hout = hout
+        self._tabsize = 2
+        self._width = 78
+        self._level = 0
+        self._split_level = 0
+        self._next_level = 0
+        self._split_pos = -1
+        self._line = []
+
+    def tabsize(self, t):
+        self._tabsize = t
+
+    def width(self, w):
+        self._width = w
+
+    def output(self, token):
+        self._line.append(token)
+        if len(self._build_line()) > self._width and len(self._line) > 1:
+            next_line = self._line[self._split_pos:]
+            self._line = self._line[:self._split_pos]
+            self._clear_cache()
+            print >> self.hout
+            self._split_level = 1
+            self._line = next_line
+
+    def split(self):
+        self._split_pos = len(self._line)
+
+    def _build_line(self):
+        indent = ' '*(self._level+self._split_level)*self._tabsize
+        return indent +' '.join(self._line)
+
+    def _clear_cache(self):
+        print >> self.hout, self._build_line(),
+        self._line = []
+
+    def newline(self):
+        self._clear_cache()
+        print >> self.hout
+        self._level = max(0, self._level +self._next_level)
+        self._split_level = 0
+        self._split_pos = -1
+
+    def indent(self, level=1):
+        self._next_level = level
+
+    def close(self):
+        self._clear_cache()
--- test_rib.py
+++ test_rib.py
@@ -15,9 +15,60 @@ class TestRIB(unittest.TestCase):
         self.assertEqual([["WorldBegin"]], tokens)
 
 
+class TestFormatter(unittest.TestCase):
+
+    def setUp(self):
+        self.hin = cStringIO.StringIO()
+        self.hout = rib.Formatter(self.hin)
+
+    def testEmpty(self):
+        self.hout.close()
+        self.assertEqual("", self.hin.getvalue())
+
+    def testOneLine(self):
+        self.hout.output("A")
+        self.hout.close()
+        self.assertEqual("A", self.hin.getvalue())
+
+    def testMultipleLines(self):
+        self.hout.output("A")
+        self.hout.newline()
+        self.hout.output("B")
+        self.hout.newline()
+        self.hout.output("C")
+        self.hout.close()
+        self.assertEqual("A\nB\nC", self.hin.getvalue())
+
+    def testTabs(self):
+        self.hout.tabsize(2)
+        self.hout.output("A")
+        self.hout.indent()
+        self.hout.newline()
+        self.hout.output("B")
+        self.hout.indent(-1)
+        self.hout.newline()
+        self.hout.output("C")
+        self.hout.close()
+        self.assertEqual("A\n  B\nC", self.hin.getvalue())
+
+    def testWrap(self):
+        self.hout.tabsize(1)
+        self.hout.width(5)
+        for x in xrange(0, 3):
+            self.hout.output("A")
+        self.hout.output("B")
+        self.hout.split()
+        self.hout.output("C")
+        self.hout.output("C")
+        self.hout.newline()
+        self.hout.output("D")
+        self.hout.close()
+        self.assertEqual("A A A\n B\n C C\nD", self.hin.getvalue())
+
 def retrieveTestSuite():
     suiteRIB = unittest.TestLoader().loadTestsFromTestCase(TestRIB)
-    return [suiteRIB]
+    suiteFormatter = unittest.TestLoader().loadTestsFromTestCase(TestFormatter)
+    return [suiteRIB, suiteFormatter]
 
 if __name__ == '__main__':
     unittest.main()
