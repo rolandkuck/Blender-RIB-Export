2c2bf2d40ba0aade670a4d20ef1dd36d23359281
Roland Kuck <blenderrib@roland.kuck.name>
2007-03-05 01:53:59 +0200

    Added motion blur example and fixed simple bug

--- rib.py
+++ rib.py
@@ -99,7 +99,7 @@ class MotionRIB(RIB):
             else:
                 name, tokens = self.frame_list[0][i] 
                 super(MotionRIB, self).output(name, *tokens)
-                break
+                continue
             super(MotionRIB, self).output("MotionBegin", motion_interval)
             for j in xrange(0, self.timescale):
                 name, tokens = self.frame_list[j][i] 
@@ -169,51 +169,49 @@ class Formatter(object):
 
 def main():
     import sys
-    hout = RIB(sys.stdout)
+    hout = MotionRIB(sys.stdout, 2, 0., 0.25)
     hout.RiFormat(800, 600, 1.0)
     hout.RiScreenWindow(-1, 1, -0.75, 0.75)
-    hout.RiFrameBegin(0)
-    hout.RiProjection("perspective", "fov", [49.1343426412])
-    hout.RiClipping(0.10000000149, 100.0)
-    hout.RiScale(1., 1., -1.)
-    hout.RiConcatTransform([ 0.685880541801, -0.317370116711, 0.654861867428, -0.0,
-                             0.727633714676, 0.312468618155, -0.610665559769, 0.0,
-                            -0.0108167678118, 0.89534330368, 0.445245355368, -0.0,
-                            -0.338183164597, -0.376693725586, -11.2523422241, 1.0, ])
-    hout.RiWorldBegin()
-    hout.RiCoordinateSystem("Lamp")
-    hout.RiAttributeBegin()
-    hout.RiTransform ([ -0.290864646435, 0.95517116785, -0.0551890581846, 0.0,
-                        -0.771100819111, -0.19988335669, 0.604524731636, 0.0,
-                         0.566393196583, 0.21839119494, 0.794672250748, 0.0,
-                         4.07624530792, 1.00545394421, 5.90386199951, 1.0, ])
-    hout.RiAttributeEnd()
-    hout.RiCoordinateSystem("Cube")
-    hout.RiAttributeBegin()
-    hout.RiTransform ([ 1.0, 0.0, 0.0, 0.0,
-                        0.0, 1.0, 0.0, 0.0,
-                        0.0, 0.0, 1.0, 0.0,
-                        0.0, 0.0, 0.0, 1.0, ])
-    hout.RiPointsPolygons([ 4, 4, 4, 4, 4, 4, ],
-                          [ 0, 1, 2, 3, 4, 7, 6,
-                            5, 0, 4, 5, 1, 1, 5,
-                            6, 2, 2, 6, 7, 3, 4, 0, 3, 7, ],
-                          "P", [ 1.0, 0.999999940395, -1.0, 1.0,
-                                -1.0, -1.0, -1.00000011921, -0.999999821186,
-                                -1.0, -0.999999642372, 1.00000035763, -1.0,
-                                1.00000047684, 0.999999463558, 1.0, 0.999999344349,
-                                -1.00000059605, 1.0, -1.00000035763, -0.999999642372,
-                                1.0, -0.999999940395, 1.0, 1.0, ])
-    hout.RiAttributeEnd()
-    hout.RiCoordinateSystem("Camera")
-    hout.RiAttributeBegin()
-    hout.RiTransform ([ 0.685880541801, 0.727633774281, -0.0108167808503, 0.0,
-                        -0.317370116711, 0.312468618155, 0.895343244076, 0.0,
-                        0.654861867428, -0.610665619373, 0.445245355368, 0.0,
-                        7.48113155365, -6.50763988495, 5.34366512299, 1.0, ])
-    hout.RiAttributeEnd()
-    hout.RiWorldEnd()
-    hout.RiFrameEnd()
+    for frame in xrange(0, 2):
+        hout.RiFrameBegin(frame)
+        hout.RiProjection("perspective", "fov", [49.1343426412])
+        hout.RiClipping(0.10000000149, 100.0)
+        hout.RiScale(1., 1., -1.)
+        hout.RiConcatTransform([ 0.685880541801, -0.317370116711, 0.654861867428, -0.0,
+                                 0.727633714676, 0.312468618155, -0.610665559769, 0.0,
+                                -0.0108167678118, 0.89534330368, 0.445245355368, -0.0,
+                                -0.338183164597, -0.376693725586, -11.2523422241, 1.0, ])
+        hout.RiWorldBegin()
+        hout.RiCoordinateSystem("Lamp")
+        hout.RiAttributeBegin()
+        hout.RiTransform ([ -0.290864646435, 0.95517116785, -0.0551890581846, 0.0,
+                            -0.771100819111, -0.19988335669, 0.604524731636, 0.0,
+                             0.566393196583, 0.21839119494, 0.794672250748, 0.0,
+                             4.07624530792, 1.00545394421, 5.90386199951, 1.0, ])
+        hout.RiAttributeEnd()
+        hout.RiCoordinateSystem("Cube")
+        hout.RiAttributeBegin()
+        hout.RiTranslate(frame, frame, frame)
+        hout.RiPointsPolygons([ 4, 4, 4, 4, 4, 4, ],
+                              [ 0, 1, 2, 3, 4, 7, 6,
+                                5, 0, 4, 5, 1, 1, 5,
+                                6, 2, 2, 6, 7, 3, 4, 0, 3, 7, ],
+                              "P", [ 1.0, 0.999999940395, -1.0, 1.0,
+                                    -1.0, -1.0, -1.00000011921, -0.999999821186,
+                                    -1.0, -0.999999642372, 1.00000035763, -1.0,
+                                    1.00000047684, 0.999999463558, 1.0, 0.999999344349,
+                                    -1.00000059605, 1.0, -1.00000035763, -0.999999642372,
+                                    1.0, -0.999999940395, 1.0, 1.0, ])
+        hout.RiAttributeEnd()
+        hout.RiCoordinateSystem("Camera")
+        hout.RiAttributeBegin()
+        hout.RiTransform ([ 0.685880541801, 0.727633774281, -0.0108167808503, 0.0,
+                            -0.317370116711, 0.312468618155, 0.895343244076, 0.0,
+                            0.654861867428, -0.610665619373, 0.445245355368, 0.0,
+                            7.48113155365, -6.50763988495, 5.34366512299, 1.0, ])
+        hout.RiAttributeEnd()
+        hout.RiWorldEnd()
+        hout.RiFrameEnd()
     hout.close()
 
 if __name__ == "__main__":
