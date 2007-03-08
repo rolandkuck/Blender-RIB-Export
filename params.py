9c35a8b63388be6dbda8fd3b747f08e7ece033ad
Roland Kuck <blenderrib@roland.kuck.name>
2007-03-09 00:49:19 +0200

    Added String, Vector and Normal parameters

--- params.py
+++ params.py
@@ -21,8 +21,39 @@ la = Blender.Lamp.Get
 # Define possible shader types
 #
 
-class Float(object):
-    def __init__(self, value):
-        self.value = value
+class _Parameter(object):
     def inline(self, name):
-        return [ "uniform float %s" % str(name), [float(self.value)] ]
+        return [ "uniform %s %s" % (self.type, str(name)), self.values ]
+
+class Float(_Parameter):
+    def __init__(self, value):
+        self.values = [ float(value) ]
+        self.type = "float"
+
+class String(_Parameter):
+    def __init__(self, value):
+        self.values = [ str(value) ]
+        self.type = "string"
+
+class _Float3(_Parameter):
+    def __init__(self, *args):
+        try:
+            if len(args) == 1:
+                values = args[0]
+            else:
+                values = args
+            if len(values) != 3:
+                raise TypeError()
+            self.values = [ float(v) for v in values ]
+        except:
+            raise TypeError()
+
+class Vector(_Float3):
+    def __init__(self, *args):
+        super(Vector, self).__init__(*args)
+        self.type = "vector"
+
+class Normal(_Float3):
+    def __init__(self, *args):
+        super(Normal, self).__init__(*args)
+        self.type = "normal"
