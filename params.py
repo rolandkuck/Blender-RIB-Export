f6762079f8c5b1c1c7e40a8ed7918b80b26ab13a
Roland Kuck <blenderrib@roland.kuck.name>
2007-03-09 00:15:36 +0200

    Added predefined functions and modules for the evaluation context

--- params.py
+++ params.py
@@ -1,3 +1,26 @@
+#
+# Imitate PyDrivers
+#
+
+import Blender
+b = blender = Blender
+
+import Blender.Noise as noise
+n = noise
+
+import math
+m = math
+
+ob = Blender.Object.Get
+me = Blender.Mesh.Get
+ma = Blender.Material.Get
+la = Blender.Lamp.Get
+
+
+#
+# Define possible shader types
+#
+
 class Float(object):
     def __init__(self, value):
         self.value = value
